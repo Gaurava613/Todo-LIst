d16235672ba563dae332cc063acb7b4df6bebb24
Gaurava613 <gauravgaikwad8112002@gmail.com>
2023-09-09 18:00:28 +0530

    initial commit

---
+++ class.js
@@ -0,0 +1,17 @@
+class Student{
+    
+    constructor(id1){
+        this.id1=id1
+      
+    }
+    display(){return`
+    <tr>
+    <td> ${this.id1} </td>
+   
+    </tr> 
+    <br>
+    `}
+
+    
+}
+
